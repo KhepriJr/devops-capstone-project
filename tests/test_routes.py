bef4dc3535412ff450712559a083c12f6b4d944f
kheprijr <matthewsben91@gmail.com>
2024-04-05 10:58:10 -0400

    fixed linting

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -28,6 +28,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
+
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
@@ -196,4 +197,3 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-        
