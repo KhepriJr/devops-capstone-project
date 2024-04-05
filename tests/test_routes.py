1b44e4cb91647ab9b4aabd57120a36b67de7a618
kheprijr <matthewsben91@gmail.com>
2024-04-05 11:00:27 -0400

    fixed linting

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -26,9 +26,9 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 
 class TestAccountService(TestCase):
+
+    
     """Account Service Tests"""
-
-
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
